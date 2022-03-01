061c1692008f86ce2b8181de854309da860c734e
osaguild <nobuhisa.abe.0922@gmail.com>
2022-03-01 13:49:21 +0900

    complete

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <div id="counter">
-    {{ message }}
+  <div class="demo">
+    <span v-bind:title="message">
+      Hover your mouse over me for a few seconds to see my dynamically-bound title!
+    </span>
   </div>
 </template>
 
@@ -8,8 +10,21 @@
 export default {
   data() {
     return {
-      message: "Hello World"
+      message: 'You loaded this page on ' + new Date().toLocaleString()
     }
   }
 }
 </script>
+
+<style>
+.demo {
+  font-family: sans-serif;
+  border: 1px solid #eee;
+  border-radius: 2px;
+  padding: 20px 30px;
+  margin-top: 1em;
+  margin-bottom: 40px;
+  user-select: none;
+  overflow-x: auto;
+}
+</style>
