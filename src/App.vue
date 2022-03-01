180e0a3749a0fec21babc521b3322d77de68c43a
osaguild <nobuhisa.abe.0922@gmail.com>
2022-03-01 14:03:32 +0900

    complete

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
-  <div id="counter">
-    {{ message }}
-  </div>
+  <p>{{ message }}</p>
+  <button v-on:click="reverseMessage">Reverse Message</button>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      message: "Hello World"
+      message: 'Hello Vue.js!'
+    }
+  },
+  methods: {
+    reverseMessage() {
+      this.message = this.message
+        .split('')
+        .reverse()
+        .join('')
     }
   }
 }
