c41947e3dbd4b027ab490ce10ee5ba4378577b8f
osaguild <nobuhisa.abe.0922@gmail.com>
2022-03-02 13:39:10 +0900

    complete

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
-  <div id="counter">
-    {{ message }}
+  <div>
+    <p>has published books</p>
+    <div>{{ publishedBooksMessageCompute }}</div>
+    <div>{{ publishedBooksMessageMethod() }}</div>
+  </div>
+  <div>
+    <p>now date</p>
+    <div>{{ dateNowCompute }}</div>
+    <div>{{ dateNowMethod() }}</div>
   </div>
 </template>
 
@@ -8,7 +15,30 @@
 export default {
   data() {
     return {
-      message: "Hello World"
+      author: {
+        name: "John Doe",
+        books: [
+          'Vue 2 - Advanced Guide',
+          'Vue 3 - Basic Guide',
+          'Vue 4 - The Mystery'
+        ]
+      }
+    }
+  },
+  computed: {
+    publishedBooksMessageCompute() {
+      return this.author.books.length > 0 ? 'Yes' : 'No'
+    },
+    dateNowCompute() {
+      return Date.now()
+    }
+  },
+  methods: {
+    publishedBooksMessageMethod() {
+      return this.author.books.length > 0 ? 'Yes' : 'No'
+    },
+    dateNowMethod() {
+      return Date.now()
     }
   }
 }
