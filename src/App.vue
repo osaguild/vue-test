2a7b57094dd6c979aae5205849bd0a385b1761fc
osaguild <nobuhisa.abe.0922@gmail.com>
2022-03-04 19:34:41 +0900

    complete

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,37 @@
 <template>
-  <div id="counter">
-    {{ message }}
-  </div>
+  <ul>
+    <li v-for="(item, index) in items" :key="index">
+      {{ parentMessage }} - {{ index }} - {{ item.message }}
+    </li>
+    <br>
+    <li v-for="(value,index) in myObject" :key="index">
+      {{ value }}
+    </li>
+    <br>
+    <li v-for="(value,name) in myObject" :key="name">
+      {{ name }}: {{ value }}
+    </li>
+    <br>
+    <li v-for="(value,name,index) in myObject" :key="index">
+      {{ index }}. {{ name }}: {{ value }}
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      message: "Hello World"
+      parentMessage: 'parent',
+      items: [
+          { message: 'Foo'},
+          { message: 'Bar'}
+      ],
+      myObject: {
+        title: 'How to do lists in Vue',
+        author: 'Jane Doe',
+        publishedAt: '2016-04-10'
+      }
     }
   }
 }
