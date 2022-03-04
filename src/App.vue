ae21743aa71ddf1045bda372fc06da44b1a7512e
osaguild <nobuhisa.abe.0922@gmail.com>
2022-03-04 19:44:57 +0900

    complete

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
-  <div id="counter">
-    {{ message }}
-  </div>
+  <ul>
+    <li v-for="n in evenNumbers" :key="n">{{ n }}</li>
+  </ul>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      message: "Hello World"
+      numbers: [1,2,3,4,5]
+    }
+  },
+  computed: {
+    evenNumbers() {
+      return this.numbers.filter(number => number % 2 === 0)
     }
   }
 }
