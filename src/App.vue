e2b829998203d9fa11b04f3d619dda0e0165e74f
osaguild <nobuhisa.abe.0922@gmail.com>
2022-03-04 19:02:08 +0900

    complete

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
-  <div id="counter">
-    {{ message }}
-  </div>
+  <h1 v-if="awesome">Vue is awesome!</h1>
+  <h1 v-else>Oh no</h1>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      message: "Hello World"
+      awesome: false
     }
   }
 }
