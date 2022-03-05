d7702cc6fcb04455a0ba5dadbb976c2afbadd90f
osaguild <nobuhisa.abe.0922@gmail.com>
2022-03-05 09:57:41 +0900

    complete

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,42 @@
 <template>
-  <div id="counter">
-    {{ message }}
-  </div>
+  <button @click="counter += 1">add 1</button>
+  <p>the button above has been clicked {{counter}} times.</p>
+  <button @click="greet">Greet</button>
+  <button @click="say('hi')">say hi</button>
+  <button @click="say('what')">say what</button>
+  <button @click="warn('form cannot be submitted yet.',$event)">submit</button>
+  <button @click="one($event), two($event)">one two</button>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      message: "Hello World"
+      counter: 0,
+      name: 'Vue.js'
+    }
+  },
+  methods: {
+    greet(event) {
+      alert('hello ' + this.name + '!')
+      if (event) {
+        alert(event.target.tagName)
+      }
+    },
+    say(message) {
+      alert(message)
+    },
+    warn(message,event) {
+      if(event){
+        event.preventDefault()
+      }
+      alert(message)
+    },
+    one(event){
+      alert('one:' + event.target.tagName)
+    },
+    two(event){
+      alert('two:' + event.target.tagName)
     }
   }
 }
