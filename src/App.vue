20b87bdd95028aaf33457ad018d7bb6a37a138ca
osaguild <nobuhisa.abe.0922@gmail.com>
2022-03-05 12:56:08 +0900

    complete

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,14 @@
 <template>
-  <div id="dynamic-component-demo" class="demo">
-    <button v-for="(tab,index) in tabs" :key="index" :class="['tag-button',{active: currentTab === tab}]" v-on:click="currentTab = tab">{{tab}}</button>
-    <component v-bind:is="currentTabComponent" class="tab"></component>
-  </div>
+  <my-component :propA="1" :propB="b" :propC="c" :propF="success"></my-component>
 </template>
 
 <script>
-import TabHome from './components/HomeComponent.vue'
-import TabPosts from './components/PostsComponent.vue'
-import TabArchive from './components/ArchiveComponent.vue'
+import MyComponent from './components/MyComponent.vue'
 
 export default {
   name: 'App',
-  data() {
-    return {
-      currentTab: 'Home',
-      tabs: ['Home','Posts','Archive']
-    }
-  },
-  computed: {
-    currentTabComponent() {
-      return 'tab-' + this.currentTab.toLowerCase()
-    }
-  },
   components: {
-    TabHome,
-    TabPosts,
-    TabArchive
+    MyComponent
   }
 }
 </script>
-<style>
-.demo {
-  font-family: sans-serif;
-  border: 1px solid #eee;
-  border-radius: 2px;
-  padding: 20px 30px;
-  margin-top: 1em;
-  margin-bottom: 40px;
-  user-select: none;
-  overflow-x: auto;
-}
-
-.tab-button {
-  padding: 6px 10px;
-  border-top-left-radius: 3px;
-  border-top-right-radius: 3px;
-  border: 1px solid #ccc;
-  cursor: pointer;
-  background: #f0f0f0;
-  margin-bottom: -1px;
-  margin-right: -1px;
-}
-.tab-button:hover {
-  background: #e0e0e0;
-}
-.tab-button.active {
-  background: #e0e0e0;
-}
-.demo-tab {
-  border: 1px solid #ccc;
-  padding: 10px;
-}
-</style>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>prop A:{{propA}}</div>
+  <div>prop B:{{propB}}</div>
+  <div>prop C:{{propC}}</div>
+  <div>prop D:{{propD}}</div>
+  <div>prop E:{{propE}}</div>
+  <div>prop F:{{propF}}</div>
+  <div>prop G:{{propG}}</div>
+</template>
+
+<script>
+export default {
+  name: "MyComponent",
+  props: {
+    propA: Number,
+    propB: [Number, String],
+    propC: {
+      type: String,
+      required: true
+    },
+    propD: {
+      type: Number,
+      default: 100
+    },
+    propE: {
+      type: Object,
+      default() {
+        return { message: 'hello'}
+      }
+    },
+    propF: {
+      validator(value) {
+        return ['success','warning','danger'].includes(value)
+      }
+    },
+    propG: {
+      type: Function,
+      dafault() {
+        return 'Default function'
+      }
+    }
+  }
+}
+</script>
